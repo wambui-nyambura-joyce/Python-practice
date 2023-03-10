fbdc945c5833f4f002a06349406019ca1485d7a1
wambui-nyambura-joyce <nyamburawambuui@gmail.com>
2023-03-10 09:24:51 +0300

    strings

---
+++ strings.py
@@ -0,0 +1,16 @@
+course = "  Python programing"
+print(course)
+print(course.upper())
+print(course.lower())
+print(course.title())
+print(course.rstrip())
+print(course.find("Pro"))
+print(course.replace("p", "j"))
+print("pro" in course)
+print("swift" not in course)
+first = "Joyce"
+last = "Nyambura"
+#full = first + " " +last
+full = f"{len(first)} {3 + 2}"
+print(full)
+#courses = "Python Programming"
